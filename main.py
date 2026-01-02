cc58ece2cf306d9363948f2d2fb55568cb8aea7d
stu <stueynz@gmail.com>
2026-01-02 14:40:06 +1300

    finished bookbot

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from stats import get_book_text, counter
+import sys
+from stats import get_book_text, counter, charcount, sorted_chars
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    num_words = counter(text)
-    print(f"Found {num_words} total words")
+    sys_arg_len = len(sys.argv)
+    print(f"sys_arg_len: {sys_arg_len}")
+    if sys_arg_len < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    text = get_book_text(sys.argv[1])
+    num_words = counter(text)
+    characters = charcount(text)
+    items = sorted_chars(characters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {str(sys.argv[1])}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in items:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -2,8 +2,32 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
+
 def counter(file_contents):
     words = 0
     for word in file_contents.split():
         words += 1
     return words
+
+def charcount(file_contents):
+    chars = {}
+    for char in file_contents.lower():
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+
+def sorted_chars(characters):
+    char_list = []
+    for char in characters:
+        if char.isalpha():
+            num = int(characters[char])
+            char_dict = {'char': char, 'num': num}
+            char_list.append(char_dict)
+
+    def sort_on(items):
+        return items["num"]
+
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
